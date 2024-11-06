1a7d180cae5b7fc042fab97a2a358553d17e5b64
pchuan98 <haeer98@outlook.com>
2024-11-06 20:06:13 +0800

    feat: base mint func

--- funcs/Kas.Func.Mint/Controllers/MintController.cs
+++ funcs/Kas.Func.Mint/Controllers/MintController.cs
@@ -1,7 +1,9 @@
-﻿using Chuan.Core.Models;
+﻿using Chuan.Core;
+using Chuan.Core.Models;
 using KasTools.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace Kas.Func.Mint.Controllers;
 
@@ -12,11 +14,15 @@ public class MintController : ControllerBase
     [HttpGet]
     public IActionResult Main()
     {
-        var mints = MintUtil.QueryAllMint();
-        
+        //var mints = MintUtil.QueryAllMint();
 
-        return Ok();
+
+        return Ok("okk");
     }
+
+    private static string ?_mintString = null;
+
+    private static DateTime _mintTime = DateTime.Now;
 
     /// <summary>
     /// 处理函数
@@ -26,7 +32,84 @@ public class MintController : ControllerBase
     [HttpPost]
     public async Task MintStart([FromBody] CallbackModel request)
     {
-        // todo 开启自动mint追踪功能
+        if (_mintString is not null && (DateTime.Now - _mintTime) < TimeSpan.FromMinutes(30))
+        {
+            await request.SendMessage(_mintString);
+
+            return;
+        }
+
+        // trending 
+        var trending = await TrendingUtils.QueryAll();
+        var trendingTickers = trending
+            ?.MostMinted
+            ?.Select(item => item.Ticker!)
+            ?.ToHashSet();
+
+        // tokens
+        var tokens = await TokenUtil.QueryAll();
+        var tokensTickers = tokens
+            ?.Where(item => item.Status?.ToLower() == "deployed")
+            .Where(item => (DateTime.Now - item.DeployedAt) < TimeSpan.FromHours(3))
+            .Select(item => item.Ticker!)
+            .ToHashSet();
+
+        trendingTickers ??= new HashSet<string>();
+        var tickers = new HashSet<string>(tokensTickers
+            ?.Union(trendingTickers) ?? Array.Empty<string>());
+
+        var infos = new List<string>();
+
+        foreach (var ticker in tickers)
+        {
+            try
+            {
+                var info = await MintManager.ParseMint(ticker, tokens: tokens);
+
+                infos.Add(info.ToString());
+                Log.Debug(info.ToString());
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+            }
+        }
+
+        var msg = string.Join("\n=======================\n", infos);
+        _mintString = msg;
+        await request.SendMessage(msg);
     }
+
+}
+
+public static class WeChatApiUtils
+{
+    public static string WeChatApiBaseUrl = "http://122.152.227.199:5099/api";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="wxid"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public static async Task<string?> SendMessage(string wxid, string content)
+    {
+        var result = await ClientUtils.Post($"{WeChatApiBaseUrl}/send", new SendModel()
+        {
+            Receiver = wxid,
+            Content = content
+        });
+
+        return result;
+    }
+
+    /// <summary>
+    /// 快速发消息
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public static async Task<string?> SendMessage(this CallbackModel model, string content)
+        => await SendMessage(model.IsGroup ? model.Group : model.Sender, content);
 
 }
--- funcs/Kas.Func.Mint/Program.cs
+++ funcs/Kas.Func.Mint/Program.cs
@@ -3,14 +3,18 @@ using Chuan.Core.Models;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
-    //.MinimumLevel.Verbose()
+    .MinimumLevel.Verbose()
     .WriteTo.Console()
     .CreateLogger();
 
 // зЂВс command
 var commands = new List<RegisterModel>()
 {
-    new("/mint","http://122.152.227.199:5098/api/price")
+#if DEBUG
+    new("mint","http://100.113.117.122:5097/api/mint")
+#else
+        new("mint","http://122.152.227.199:5097/api/mint")
+#endif
 };
 
 commands.ForEach(command =>
